e80c28f8fa270c300aa99f0ec234fb91d4cef859
Gabriel Silva da Cruz <gabriel.cruz@cs2consultoria.com.br>
2022-01-16 12:59:47 -0300

    Project created

---
+++ backend/src/main/java/br/com/g81/cinevaluerg81/Cinevaluerg81Application.java
@@ -0,0 +1,13 @@
+package br.com.g81.cinevaluerg81;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cinevaluerg81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cinevaluerg81Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/g81/cinevaluerg81/Cinevaluerg81ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.g81.cinevaluerg81;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cinevaluerg81ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
